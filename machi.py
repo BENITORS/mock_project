534d2a2470dc453f985a7a6d9f10553d20419e0b
BENITORS <bruvalcaba29@gmail.com>
2022-08-05 17:59:24 -0500

    estructura actualizada

--- machi.py
+++ machi.py
@@ -2,6 +2,11 @@ import unittest
 import selenium
 from selenium import webdriver
 import HtmlTestRunner
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import os
+import time
 
 class mockTesting(unittest.TestCase):
     def setUp(self) -> None:
